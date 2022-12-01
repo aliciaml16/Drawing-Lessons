cc0150fe245dbf25c1c3ac5e9229697092856c09
Alicia Martínez López <alicialicia9816@gmail.com>
2022-12-01 12:24:34 +0100

    Upload

---
+++ Assets/Our Game/Scripts/ModelChange.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelChange : MonoBehaviour
+{
+    public GameObject[] buttons;
+    public GameObject[] objects_step1;
+    public GameObject[] objects_step2;
+    public GameObject[] objects_step3;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
