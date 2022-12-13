ebbc128d3dc2fae75b9541fe129c51c020a761fb
Alicia Martínez López <alicialicia9816@gmail.com>
2022-12-13 16:00:31 +0100

    Models implemented

--- Assets/Our Game/Scripts/ModelChange.cs
+++ Assets/Our Game/Scripts/ModelChange.cs
@@ -22,11 +22,11 @@ public class ModelChange : MonoBehaviour
 
     void OnEnable()
     {
-        clone00 = Instantiate(steps[0], positionSteps[0].transform.position, Quaternion.identity);
+        clone00 = Instantiate(steps[0], positionSteps[0].transform.position, Quaternion.Euler(0f, 0f, 0f));
         clone00.transform.parent = positionSteps[0].transform;
-        clone01 = Instantiate(steps[1], positionSteps[1].transform.position, Quaternion.identity);
+        clone01 = Instantiate(steps[1], positionSteps[1].transform.position, Quaternion.Euler(0f, 0f, 0f));
         clone01.transform.parent = positionSteps[1].transform;
-        clone02 = Instantiate(steps[2], positionSteps[2].transform.position, Quaternion.identity);
+        clone02 = Instantiate(steps[2], positionSteps[2].transform.position, Quaternion.Euler(0f, 0f, 0f));
         clone02.transform.parent = positionSteps[2].transform;
     }
 
